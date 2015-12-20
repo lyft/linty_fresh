351d09ce66a1389f6af503d6a140fd2ff4b646eb
Roy Williams <rwilliams@lyft.com>
2015-12-19 17:18:09 -0800

    Test travis ci integration

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,9 +3,6 @@
 set -e
 python setup.py test
 
-GITHUB_SLUG="$(git config --get remote.origin.url | \
-  sed 's/git@github.com*:\([^\.]*\)\.git/\1/')"
-
 # This is a bit of a hack.  Travis tests pushes to branches and pull
 # requests.  Unfortunately, the pull request tests are testing the merge
 # commit between the PR and master.  This is desirable for unit tests,
@@ -25,10 +22,10 @@ LINT_RESULT=$?
 
 if [ -n "${TRAVIS_PULL_REQUEST}" ] && \
    [ "${TRAVIS_PULL_REQUEST}" != 'false' ] && \
-   [ -n "${GITHUB_SLUG}" ] && \
+   [ -n "${TRAVIS_REPO_SLUG}" ] && \
    [ -n "${GITHUB_AUTH_TOKEN}" ]; then
     COMMIT="$(git rev-parse HEAD)"
-    PR_URL="https://github.com/${GITHUB_SLUG}/pull/${TRAVIS_PULL_REQUEST}"
+    PR_URL="https://github.com/${TRAVIS_REPO_SLUG}/pull/${TRAVIS_PULL_REQUEST}"
     linty_fresh --pr_url "${PR_URL}" --commit "${COMMIT}" \
                 --linter pylint flake8.txt
 fi
