296db4b9c6fc2fc9598dd0ca0974882c6e59045b
Zach Perrault <zach.perrault@gmail.com>
2015-12-23 15:30:34 -0500

    Adds support for parsing eslint compact output

---
+++ linty_fresh/linters/eslint.py
@@ -0,0 +1,18 @@
+import re
+
+from linty_fresh.problem import Problem
+
+from typing import Set
+
+ESLINT_LINE_REGEX = re.compile(r'(?P<path>[^:]*): line (?P<line>\d*), col (?P<column>\d*), (?P<message>.*)')
+
+def parse(contents: str) -> Set[Problem]:
+  result = set()  # type: Set[Problem]
+  for line in contents.splitlines():
+    match = ESLINT_LINE_REGEX.match(line)
+    if match:
+      groups = match.groupdict()
+      result.add(Problem(groups['path'],
+                         groups['line'],
+                         groups['message']))
+  return result 
