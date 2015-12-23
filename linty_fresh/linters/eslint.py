cb72656aa5772f62a4e727a13da01e4e49597a5e
Zach Perrault <zach.perrault@gmail.com>
2015-12-23 16:27:30 -0500

    Fixes linting errors

--- linty_fresh/linters/eslint.py
+++ linty_fresh/linters/eslint.py
@@ -4,15 +4,17 @@ from linty_fresh.problem import Problem
 
 from typing import Set
 
-ESLINT_LINE_REGEX = re.compile(r'(?P<path>[^:]*): line (?P<line>\d*), col (?P<column>\d*), (?P<message>.*)')
+ESLINT_LINE_REGEX = re.compile(r'(?P<path>[^:]*): line (?P<line>\d*), '
+                               r'col (?P<column>\d*), (?P<message>.*)')
+
 
 def parse(contents: str) -> Set[Problem]:
-  result = set()  # type: Set[Problem]
-  for line in contents.splitlines():
-    match = ESLINT_LINE_REGEX.match(line)
-    if match:
-      groups = match.groupdict()
-      result.add(Problem(groups['path'],
-                         groups['line'],
-                         groups['message']))
-  return result 
+    result = set()  # type: Set[Problem]
+    for line in contents.splitlines():
+        match = ESLINT_LINE_REGEX.match(line)
+        if match:
+            groups = match.groupdict()
+            result.add(Problem(groups['path'],
+                               groups['line'],
+                               groups['message']))
+    return result
