45fe650376167c776502cbae0c9201be8bdcdfae
Keith Smiley <keithbsmiley@gmail.com>
2018-09-24 15:32:05 -0700

    Update xcodebuild regex to exclude some logs (#42)
    * Update xcodebuild regex to exclude some logs
    
    In Xcode 10 there are some new logs that matched the looser version of
    this regex, even though they weren't errors. Instead we now limit the
    valid level matches.

--- tests/linters/test_xcodebuild.py
+++ tests/linters/test_xcodebuild.py
@@ -11,6 +11,7 @@ class XcodebuildTest(unittest.TestCase):
     def test_parse_errors(self):
         test_string = [
             "<unknown>:0: error: no such file or directory: 'foo.swift'",
+            "<unknown>:0: ERROR: no such file or directory: 'case.swift'",
             "{}/Classes/foo/bar baz/qux.swift:201:21: "
             "error: use of unresolved identifier 'FooBar'"
             .format(os.path.curdir),
@@ -24,12 +25,18 @@ class XcodebuildTest(unittest.TestCase):
         ]
 
         result = xcodebuild.parse('\n'.join(test_string))
-        self.assertEqual(4, len(result))
+        self.assertEqual(5, len(result))
 
         self.assertIn(
             Problem('<unknown>',
                     0,
                     "no such file or directory: 'foo.swift'"),
+            result)
+
+        self.assertIn(
+            Problem('<unknown>',
+                    0,
+                    "no such file or directory: 'case.swift'"),
             result)
 
         self.assertIn(
@@ -54,3 +61,13 @@ class XcodebuildTest(unittest.TestCase):
                     'constraint attributes. This may produce unexpected '
                     'results at runtime before Xcode 5.1'),
             result)
+
+    def test_should_not_parse(self):
+        test_string = [
+            "Details:  log recorder was sent "
+            "-stopRecordingWithInfo:completionBlock: after it had already "
+            "been asked to stop recording.",
+        ]
+
+        result = xcodebuild.parse('\n'.join(test_string))
+        self.assertEqual(0, len(result))
