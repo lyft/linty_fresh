c7c676f2f5b8b058124dea3fa210091130290587
Keith Smiley <keithbsmiley@gmail.com>
2018-09-24 14:41:14 -0700

    Ignore case entirely

--- linty_fresh/linters/xcodebuild.py
+++ linty_fresh/linters/xcodebuild.py
@@ -7,8 +7,9 @@ from linty_fresh.problem import Problem
 XCODEBUILD_LINE_REGEX = re.compile(
     r'(?P<path>[^:]*):'
     r'((?P<line>\d*)|(?P<reference>[^:]*)):'
-    r'(?:(?P<column>\d*):)?\s*(?P<level>((?i)error|warn(ing)?|note|info)):'
-    r'\s*(?P<message>.*)'
+    r'(?:(?P<column>\d*):)?\s*(?P<level>(error|warn(ing)?|note|info)):'
+    r'\s*(?P<message>.*)',
+    re.IGNORECASE,
 )
 
 
